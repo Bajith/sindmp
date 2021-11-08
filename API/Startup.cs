2dd62dcf693d0d7b3cd88fda52df21c30badf26b
AJITH <ajith@AJITHs-MacBook-Pro.local>
2021-11-08 23:48:33 +0530

    fouth commit

--- API/Startup.cs
+++ API/Startup.cs
@@ -15,6 +15,9 @@ using Microsoft.EntityFrameworkCore;
 using Infrastructure.Data;
 using Core.Interfaces;
 using API.Helpers;
+using API.Middleware;
+using API.Errors;
+using API.Extensions;
 
 namespace API
 {
@@ -29,28 +32,28 @@ namespace API
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddScoped<IProductRepository, ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>), (typeof(GenericRepository<>)));
             services.AddAutoMapper(typeof(MappingProfiles));
             services.AddControllers();
             services.AddDbContext<StoreContext>(x => x.UseSqlite(_config.GetConnectionString("DefaultConnection")));
 
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
+            services.AddApplicationServices();
+
+            services.AddSwaggerDocumentation();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                //app.UseDeveloperExceptionPage();
+                app.UseSwaggerDocumentation();
             }
 
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+            
             app.UseHttpsRedirection();
 
             app.UseRouting();
